6947eda869109349a32aac0b91dcd58c069534e2
rama41222 <dinushankanrg@gmail.com>
2018-07-29 08:37:35 +0530

    mvp stage

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const socketio = require('socket.io')
 const http = require('http')
+const uuid = require('uuid')
 const express = require('express')
 const bodyParser = require('body-parser')
 const { defaultConfig } = require('./config/index')
@@ -16,8 +17,37 @@ io.on('connection', function (client) {
     
     client.on('register', function (data) {
         setClient(client.id, data.name)
-        client.emit('user',{ hasRegistered: true, user: getClientById(client.id) } )
+        client.emit('user',{ hasRegistered: true, user: {id: client.id, name:getClientById(client.id)} } )
         io.emit('userlist',{ users: getClients() } )
+    })
+    
+    client.on('subscribe', function (data) {
+        console.log('executed ......')
+        console.log(data)
+        console.log('executed ......')
+        
+        client.join(data.roomid)
+        if(typeof data.user.id === 'undefined' || !data.user) {
+            return
+        }
+        io.sockets.connected[data.user.id].emit('request', { roomid: data.roomid , user: data.user});
+    })
+    
+    client.on('join-room', function (data) {
+        client.join(data.roomid)
+    })
+    
+    client.on('private-chat', function (data) {
+        client.broadcast.to(data.roomid).emit('privatemessage', data.message)
+    })
+    
+    client.on('create-private-room', function (data) {
+        if(client.id !== data.self.id) {
+            client.emit('error', 'Unauthorized Client')
+            return
+        }
+        const roomId = uuid()
+        client.emit('joinroom', { roomid: roomId, user: data.user} )
     })
     
     client.on('disconnect', function () {
